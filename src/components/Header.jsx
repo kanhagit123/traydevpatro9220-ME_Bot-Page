bd10bcae17f4c3947e302566170dddec181ef1db
Kanha Patro <kanhapatro@Kanhas-MacBook-Air.local>
2025-09-07 13:11:31 +0530

    hedaerjsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export default function Header() {
   return (
     <header className="app-header">
-      <h1>Bot AI</h1>
+     
+      <span>Soul AI</span>
       <nav>
         <Link to="/">Chat</Link>
         <Link to="/history">History</Link>
         <Link to="/feedback">Feedback</Link>
-        {/* New Chat button should be an <a> link */}
+       
         <a href="/">New Chat</a>
       </nav>
     </header>
